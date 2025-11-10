ad1ec4bd05e570a4f24c3c82d234de03a34c9608
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2025-11-10 09:29:36 -0400

    feat: env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { Module } from "@nestjs/common";
 import { PrismaService } from "./prisma/prisma.service";
 import { ConfigModule } from "@nestjs/config";
 import { CreateAccountController } from "./controllers/create-account.controller";
+import { envSchema } from "./env";
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (env) => envSchema.parse(env),
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
