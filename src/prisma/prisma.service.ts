4791fd47f64ceaa468fb0c09135c4a2fc3c02bf1
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2025-11-10 08:30:49 -0400

    feat: prisma service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "generated/prisma/client";
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({ log: ["warn", "error"] });
+  }
+
+  onModuleInit() {
+    return this.$connect();
+  }
+
+  onModuleDestroy() {
+    return this.$disconnect();
+  }
+
+  getUsers() {
+    return this.user.findMany();
+  }
+}
